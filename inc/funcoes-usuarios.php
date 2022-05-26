1263e88765a4abce0b2162c98747f56dcbcfcd3e
camargo214758 <guisafeju@gmail.com>
2022-05-26 16:59:38 -0300

    camargo.prog

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -43,7 +43,15 @@ function lerUsuarios(mysqli $conexao){
 
 
 // Função lerUmUsuario: usada em usuario-atualiza.php
+function lerUmUsuario(mysqli $conexao, int $id){
+$sql = "SELECT id, nome, email, tipo, senha FROM usuarios
+WHERE id = $id";
 
+$resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+return mysqli_fetch_assoc($resultado);
+
+}
 // fim lerUmUsuario
 
 
@@ -55,6 +63,13 @@ function lerUsuarios(mysqli $conexao){
 
 
 // Função atualizarUsuario: usada em usuario-atualiza.php
+function atualizarUsuario(mysqli $conexao, int $id, string $email, string $senha, string $tipo){
+
+    $sql = "UPDATE usuarios SET nome = '$nome', email = '$email',
+    senha = '$senha', tipo = '$tipo' WHERE id = $id";
+
+mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
 
 // fim atualizarUsuario
 
